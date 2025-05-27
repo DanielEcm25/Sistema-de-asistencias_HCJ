6c49e2e89d8692c8d8f1ffa22c9a725df1feadd8
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-05-27 10:46:40 -0500

    Primer intento de conexión a firebase

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,11 +1,11 @@
-// netlify/functions/firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./tu-archivo-de-clave.json"); // 🔐 Aquí va tu JSON
 
 if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
     admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-});
+    });
 }
 
 const db = admin.firestore();
