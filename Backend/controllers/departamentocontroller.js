94d78df875399ca136d6c260485f94b8370ab6a1
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-05-27 11:06:56 -0500

    Intento de conectar firebase

--- Backend/controllers/departamentocontroller.js
+++ Backend/controllers/departamentocontroller.js
@@ -1,7 +1,12 @@
+const db = require("../firebase");
+
 exports.consultar = async (req, res) => {
     try {
-        // Lógica para consultar el nombre del departamento
-        res.json({ nombre: "Nombre del Departamento" });
+        const doc = await db.collection("departamento").doc("info").get();
+        if (!doc.exists) {
+            return res.status(404).json({ error: "Departamento no encontrado" });
+    }
+        res.json({ nombre: doc.data().nombre });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -10,7 +15,7 @@ exports.consultar = async (req, res) => {
 exports.modificar = async (req, res) => {
     try {
         const { nuevoNombre } = req.body;
-        // Lógica para modificar el nombre del departamento
+        await db.collection("departamento").doc("info").set({ nombre: nuevoNombre });
         res.send("Departamento modificado exitosamente");
     } catch (error) {
         res.status(400).json({ error: error.message });
