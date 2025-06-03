3603423d59de3e7f41c552b4a66b642bb8a52bd9
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-06-03 11:59:42 -0500

    holaaa

--- netlify/functions/estudiantes.js
+++ netlify/functions/estudiantes.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var cors = require("cors");
-var serverless = require ('serverless-http');
-var port = process.env.PORT || 5000;
-var estudiantesroutes = require("../../Backend/routes/estudiantesroutes.js");
-var app = express();
+const express = require('express');
+const cors = require("cors");
+const serverless = require('serverless-http');
+const estudiantesroutes = require("../../Backend/routes/estudiantesroutes.js");
+const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-var router = express.Router();
-router.use ("/estudiantes",estudiantesroutes);
-var handler = app.use ('/.netlify/functions',router);
+
+app.use('/.netlify/functions/estudiantes', estudiantesroutes);
+
 module.exports.handler = serverless(app);
