c65a146b7e74e7df9d418c30c482865be58d77f1
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-06-03 10:42:04 -0500

    Intentando aun

--- netlify/functions/departamento.js
+++ netlify/functions/departamento.js
@@ -4,6 +4,23 @@ var serverless = require ('serverless-http');
 var port = process.env.PORT || 5000;
 var departamentoroutes = require("../../Backend/routes/departamentoroutes.js");
 var app = express();
+app.use((req, res, next) => {
+  let data = '';
+  req.on('data', chunk => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    try {
+      if (data) {
+        req.rawBody = data;
+        req.body = JSON.parse(data);
+      }
+    } catch (err) {
+      console.error("Error al parsear JSON:", err);
+    }
+    next();
+  });
+});
 app.use(express.json());
 app.use(cors());
 
