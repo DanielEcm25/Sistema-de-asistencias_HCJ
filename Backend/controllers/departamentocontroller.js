642dd65ea88a86a1a9c67dcf9af4336caa375ea1
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-06-03 09:54:57 -0500

    AAAA

--- Backend/controllers/departamentocontroller.js
+++ Backend/controllers/departamentocontroller.js
@@ -14,10 +14,12 @@ exports.consultar = async (req, res) => {
 
 exports.modificar = async (req, res) => {
     try {
+        console.log("Body Recibido",req.body);
         const { nuevoNombre } = req.body;
         await db.collection("Departamento").doc("Informacion").set({ Nombre: nuevoNombre });
         res.send("Departamento modificado exitosamente");
     } catch (error) {
+        console.error("Error al modificar",error.message);
         res.status(400).json({ error: error.message });
     }
 };
--- netlify/functions/departamento.js
+++ netlify/functions/departamento.js
@@ -2,8 +2,8 @@ var express = require('express');
 var cors = require("cors");
 var serverless = require ('serverless-http');
 var port = process.env.PORT || 5000;
-var app = express();
 var departamentoroutes = require("../../Backend/routes/departamentoroutes.js");
+var app = express();
 app.use(express.json());
 app.use(cors());
 
